5bbed67b6302697531736848f2c662ce54bfa0c5
etaverne <etaverne@aneo.fr>
2018-09-03 14:27:07 +0200

    feat: Deleted the lonely EntityList and placed it statically in the module feat: Enhanced reducers instanciation to create them on the fly if needed

--- src/lib/decorators/entity.decorator.ts
+++ src/lib/decorators/entity.decorator.ts
@@ -1,11 +1,12 @@
-import {entitiesList} from '../domain/entities.list';
 import {EntityDescriptor} from '../domain/descriptors';
 import {EntityManager} from '../domain/api/entity-manager';
+import {AngularRestModule} from '../angular-rest.module';
 
 export function Entity<T extends EntityDescriptor>(entityDescriptor: T): ClassDecorator {
   return function (constructor: any) {
     constructor.entityManager = new EntityManager<typeof constructor>(constructor);
     entityDescriptor.class = constructor;
-    entitiesList.push(entityDescriptor);
+
+    AngularRestModule.registerEntity(entityDescriptor);
   };
 }
